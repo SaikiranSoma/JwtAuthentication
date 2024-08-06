70d794eb06ef44d5bb5bd9eb72336ebb81930163
SaiVenkat.Chitturi <SaiVenkat.Chitturi@ust.com>
2024-08-06 10:24:47 +0530

    Added Primary Constructor

--- JWT Token Authentication/Controllers/AuthenticateController.cs
+++ JWT Token Authentication/Controllers/AuthenticateController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWT_Token_Authentication.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthenticateController : ControllerBase
+    public class AuthenticateController(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration) : ControllerBase
     {
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+        private readonly IConfiguration _configuration = configuration;
+
+
     }
 }

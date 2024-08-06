2b4fc20f6add89c0f40d2361ef512610c73bd60c
SaiVenkat.Chitturi <SaiVenkat.Chitturi@ust.com>
2024-08-06 15:26:46 +0530

    Added Login Validation Rules

--- JWT Token Authentication/Validations/LoginValidation.cs
+++ JWT Token Authentication/Validations/LoginValidation.cs
@@ -1,6 +1,16 @@
-﻿namespace JWT_Token_Authentication.Validations
+﻿using FluentValidation;
+using JWT_Token_Authentication.Models;
+
+namespace JWT_Token_Authentication.Validations
 {
-    public class LoginValidation
+    public class LoginValidation : AbstractValidator<LoginModel>
     {
+        public LoginValidation()
+        {
+            RuleFor(login => login.Username)
+                .NotEmpty().WithMessage("Username must not be empty");
+            RuleFor(login => login.Password)
+                .NotEmpty().WithMessage("Password must not be empty");
+        }
     }
 }

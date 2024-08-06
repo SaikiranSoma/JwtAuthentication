d7b8e07c906c81ce3e665b6b562465507d2802ac
Saikiran Soma <Saikiran.Soma@ust.com>
2024-08-06 10:29:53 +0530

    Configured applicationDb.

--- JWT Token Authentication/Program.cs
+++ JWT Token Authentication/Program.cs
@@ -1,5 +1,16 @@
+using JWT_Token_Authentication.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+ConfigurationManager configuration=builder.Configuration;
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+
+    options.UseSqlServer(configuration.GetConnectionString("ConnStr"))
+);
 // Add services to the container.
 
 builder.Services.AddControllers();

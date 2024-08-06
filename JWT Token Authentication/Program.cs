711440dcd3bcc87f8f0d60b74cacc3ee42b8db32
Saikiran Soma <Saikiran.Soma@ust.com>
2024-08-06 10:34:43 +0530

    Aded Identity Configuration

--- JWT Token Authentication/Program.cs
+++ JWT Token Authentication/Program.cs
@@ -1,16 +1,23 @@
 using JWT_Token_Authentication.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+//Adding applicationdbcontext 
 ConfigurationManager configuration=builder.Configuration;
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
 
     options.UseSqlServer(configuration.GetConnectionString("ConnStr"))
 );
+
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 // Add services to the container.
 
 builder.Services.AddControllers();

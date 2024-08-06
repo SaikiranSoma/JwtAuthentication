ce6e1fd2e6ad6ab48982e8324c04e7b566293232
Saikiran Soma <Saikiran.Soma@ust.com>
2024-08-06 16:17:54 +0530

    Implemented Demo functionality

---
+++ JWT Token Authentication/DemoController/DemoProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Token_Authentication.DemoController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoProductController : ControllerBase
+    {
+
+
+    }
+}

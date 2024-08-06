50182fed524f36a8d44267428a476fd0aeaeda29
Saikiran Soma <Saikiran.Soma@ust.com>
2024-08-06 10:08:11 +0530

    Implemented Dbcontext Constructor

---
+++ JWT Token Authentication/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT_Token_Authentication.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>//It is inheriting from Identitydbcontext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        
+        
+        }
+
+        
+
+
+    }
+}

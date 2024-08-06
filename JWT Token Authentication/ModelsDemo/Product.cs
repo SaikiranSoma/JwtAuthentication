16cf261d893f9728743c5300c9c10ec4545c05d3
Saikiran Soma <Saikiran.Soma@ust.com>
2024-08-06 17:34:29 +0530

    Set id as Primary key Set defaultvalue of name is emptystring Changed datatype of Price from string to double.

--- JWT Token Authentication/ModelsDemo/Product.cs
+++ JWT Token Authentication/ModelsDemo/Product.cs
@@ -1,10 +1,13 @@
-﻿namespace JWT_Token_Authentication.ModelDemo
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWT_Token_Authentication.ModelDemo
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Price { get; set; }
+        public string Name { get; set; } = "";
+        public double Price { get; set; }
 
     }
 }

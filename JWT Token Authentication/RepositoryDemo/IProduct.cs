3c2f551a904ce0ad779f6eafdef3dbd2c8510f7a
SaiVenkat.Chitturi <SaiVenkat.Chitturi@ust.com>
2024-08-06 22:52:55 +0530

    Revert master

---
+++ JWT Token Authentication/RepositoryDemo/IProduct.cs
@@ -0,0 +1,11 @@
+﻿using JWT_Token_Authentication.Models;
+
+namespace JWT_Token_Authentication.RepositoryDemo
+{
+    public interface IProduct
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product CreateProduct(Product product);
+
+    }
+}

c258114ae7741ddd05fae5132f35109a795410d6
Prasanth2003 <prasanthmukundan2003@gmail.com>
2024-08-06 17:05:47 +0530

    implemented IProduct interface

--- JWT Token Authentication/RepositoryDemo/Productservices.cs
+++ JWT Token Authentication/RepositoryDemo/Productservices.cs
@@ -1,17 +1,25 @@
-﻿using JWT_Token_Authentication.ModelDemo;
+﻿using JWT_Token_Authentication.DataDemo;
+using JWT_Token_Authentication.ModelDemo;
 
 namespace JWT_Token_Authentication.RepositoryDemo
 {
     public class Productservices : IProduct
     {
+        private readonly ProductDbContext _context;
+        public Productservices(ProductDbContext context)
+        {
+            _context = context;
+        }
         public Product CreateProduct(Product product)
         {
-            throw new NotImplementedException();
+            _context.Add(product);
+            _context.SaveChanges();
+            return product;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            throw new NotImplementedException();
+            return _context.products.ToList();
         }
     }
 }

08c522fdf533b9df77f42126674757cdef6847b5
SaiVenkat.Chitturi <saivenkat.chitturi@ust.com>
2024-08-19 15:32:43 +0530

    git ignored appsettings.json Fixed post merge bugs

--- JWT Token Authentication/Controllers/DemoController.cs
+++ JWT Token Authentication/Controllers/DemoController.cs
@@ -1,6 +1,5 @@
 ﻿using JWT_Token_Authentication.Model;
 using JWT_Token_Authentication.Models;
-using JWT_Token_Authentication.RepositoryDemo;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +9,8 @@ namespace JWT_Token_Authentication.Controllers
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class DemoController(IProduct product) : ControllerBase
+    public class DemoController : ControllerBase
     {
-        private readonly IProduct _product = product;
-
         [Authorize(Roles = UserRoles.User)]
         [HttpGet("User")]
         public IActionResult HelloUser()
